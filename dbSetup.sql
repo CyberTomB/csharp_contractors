d14ab4ef962df89e245a91ee89a0bdfec23cdf0a
Tom <afntwrk.guyrpg@gmail.com>
2021-09-07 16:51:10 -0600

    made tables

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,3 +6,25 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+
+CREATE TABLE contractor(  
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    name varchar(255) NOT NULL comment 'contractor name'
+) default charset utf8;
+CREATE TABLE job(  
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    name varchar(255) NOT NULL comment 'job name'
+) default charset utf8;
+CREATE TABLE company(  
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    name varchar(255) NOT NULL comment 'company name'
+) default charset utf8;
+
+CREATE TABLE contracts(
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    jobId int NOT NULL comment 'references job table',
+    contractorId int NOT NULL comment 'references contractor table',
+    FOREIGN KEY (jobId) REFERENCES job(id) ON DELETE CASCADE,
+    FOREIGN KEY (contractorId) REFERENCES contractor(id)
+);
